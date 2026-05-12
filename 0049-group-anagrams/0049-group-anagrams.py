8d54b48dd418a3e8209631eb5649b1c895002f50
shaniya75 <shaniyavarghese75@gmail.com>
2026-05-12 19:02:50 +0530

    Time: 11 ms (84.4%), Space: 21.7 MB (93.15%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key in group:
+                group[key].append(word)
+            else:
+                group[key] = [word]
+
+        return list(group.values())
