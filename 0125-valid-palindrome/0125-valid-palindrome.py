7d885b9da287ece371bd35ca1c971cd33439ac83
shaniya75 <shaniyavarghese75@gmail.com>
2026-05-18 20:19:25 +0530

    Time: 9 ms (41.94%), Space: 19.6 MB (56.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,11 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         dup=""
         for i in s.lower():
-            if i.isalnum():
+            if i in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789":
                 dup+=i
         rev=""
         for i in range(len(dup)-1,-1,-1):
-            if dup[i].isalnum():
+            if dup[i] in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789":
                 rev+=dup[i]
         print ( rev)
         return rev==dup
